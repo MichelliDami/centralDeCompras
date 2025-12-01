405fd8e624b81a8828ba6be256f603b871c2653c
Michelli Daminell <michelly.daminelli@gmail.com>
2025-11-30 23:01:33 -0300

    fix: corrigir nome do arquivo campaignRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-// Tudo minúsculo para evitar erro no Linux/Render
 router.use("/users", require("./usersRoutes"));
 router.use("/suppliers", require("./supplierRoutes"));
 router.use("/stores", require("./storeRoutes"));
 router.use("/products", require("./productRoutes"));
 router.use("/orders", require("./orderRoutes"));
-router.use("/campaigns", require("./campaignRoutes"));
+router.use("/campaigns", require("./campaignRoutes")); 
 
 module.exports = router;
