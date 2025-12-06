f68362d43e2f6cb26ba85b7c16567fc68e7b6045
Michelli Daminell <michelly.daminelli@gmail.com>
2025-12-06 01:20:22 -0300

    Ajustes no userSchema e swagger

--- src/models/users.js
+++ src/models/users.js
@@ -6,15 +6,31 @@ const userSchema = new mongoose.Schema(
     contact_email: { type: String, required: true },
     user: { type: String, required: true },
     pwd: { type: String, required: true },
+
     level: {
       type: String,
       enum: ["admin", "user", "manager"],
       required: true
     },
+
     status: {
       type: String,
       enum: ["on", "off"],
       required: true
+    },
+
+   
+    store_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "store",
+      default: null
+    },
+
+    
+    supplier_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "suppliers",
+      default: null
     }
   },
   { timestamps: true }
